9c1a810bf79db976c00fe857a8aee0521df02e4a
lh123cha <1491337293@qq,com>
2021-08-22 11:50:25 +0800

    Initial Commit

---
+++ cloudfunctions/EssayFunctions/getHistory/index.js
@@ -0,0 +1,22 @@
+const cloud = require('wx-server-sdk')
+
+cloud.init({
+  env: cloud.DYNAMIC_CURRENT_ENV
+})
+const db = cloud.database()
+
+// 查询数据库集合云函数入口函数
+exports.main = async (event, context) => {
+  // 返回数据库查询结果
+  try{
+    res=db.collection('eventList').where({
+      today:event.date
+    }).get();
+  }catch(exption){
+    console.log("Get Error");
+  }
+  return res;
+  // return db.collection('eventList').where({
+  //   date: event.date,
+  // }).get();
+}
